d6c0b3bb5885128d74fc4674e83367efdbd921ae
Alessandro Franceschi <al@lab42.it>
2013-08-10 14:38:54 +0200

    Richer and more complete conf define

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -2,17 +2,28 @@
 # = Define: redis::conf
 #
 # With this define you can manage any redis configuration file
-# providing a template and, eventually, and options hash where you
-# can place any redis specific setting.
+# You have 3 parameters to provide it: source, template and content.
 #
 # == Parameters
 #
 # [*template*]
-#   String. Required. Default: Provided by the module.
+#   String. Optional. Default: undef. Alternative to: source, content.
 #   Sets the module path of a custom template to use as content of
 #   the config file
-#   When defined, config file has: content => content("$template")
+#   When defined, config file has: content => content($template),
 #   Example: template => 'site/redis/my.conf.erb',
+#
+# [*content*]
+#   String. Optional. Default: undef. Alternative to: template, source.
+#   Sets directly the value of the file's content parameter
+#   When defined, config file has: content => $content,
+#   Example: content => "# File Managed by Puppet \n",
+#
+# [*source*]
+#   String. Optional. Default: undef. Alternative to: template, content.
+#   Sets the value of the file's source parameter
+#   When defined, config file has: source => $source,
+#   Example: source => 'puppet:///site/redis/my.conf',
 #
 # [*ensure*]
 #   String. Default: present
@@ -25,7 +36,12 @@
 #   The path of the created config file. If not defined a file
 #   name like the  the name of the title a custom template to use as content of configfile
 #   If defined, configfile file has: content => content("$template")
-#   Example: path => '/usr/local/etc/redis/my.conf',
+#
+# [*mode*] [*owner*] [*group*] [*notify*] [*require*] [*replace*]
+#   String. Optional. Default: undef
+#   All these parameters map directly to the created file attributes.
+#   If not defined the module's defaults are used.
+#   If defined, config file file has, for example: mode => $mode
 #
 # [*options_hash*]
 #   Hash. Default undef. Needs: 'template'.
@@ -33,11 +49,24 @@
 #   arbitrary settings.
 #
 define redis::conf (
-  $template,
-  $path         = undef,
+
+  $source       = undef,
   $template     = undef,
+  $content      = undef,
+
+  $path         = undef,
+  $mode         = undef,
+  $owner        = undef,
+  $group        = undef,
+  $notify       = undef,
+  $require      = undef,
+  $replace      = undef,
+
   $options_hash = undef,
+
   $ensure       = present ) {
+
+  validate_re($ensure, ['present','absent'], 'Valid values are: present, absent. WARNING: If set to absent the conf file is removed.')
 
   include redis
 
@@ -46,17 +75,55 @@ define redis::conf (
     default => $path,
   }
 
+  $managed_content = $content ? {
+    undef   => $template ? {
+      undef => undef,
+      default => template($template),
+    },
+    default => $content,
+  }
+
+  $managed_mode = $mode ? {
+    undef   => $redis::file_mode,
+    default => $mode,
+  }
+
+  $managed_owner = $owner ? {
+    undef   => $redis::file_owner,
+    default => $owner,
+  }
+
+  $managed_group = $group ? {
+    undef   => $redis::file_group,
+    default => $group,
+  }
+
+  $managed_require = $require ? {
+    undef   => $redis::file_require,
+    default => $require,
+  }
+
+  $managed_notify = $notify ? {
+    undef   => $redis::file_notify,
+    default => $notify,
+  }
+
+  $managed_replace = $replace ? {
+    undef   => $redis::file_replace,
+    default => $replace,
+  }
+
   file { "redis_conf_${name}":
     ensure  => $ensure,
+    source  => $source,
+    content => $managed_content,
     path    => $managed_path,
-    mode    => $redis::file_mode,
-    owner   => $redis::file_owner,
-    group   => $redis::file_group,
-    require => Package[$redis::package],
-    notify  => $redis::file_notify,
-    content => template($template),
-    replace => $redis::file_replace,
-    audit   => $redis::file_audit,
+    mode    => $managed_mode,
+    owner   => $managed_owner,
+    group   => $managed_group,
+    require => $managed_require,
+    notify  => $managed_notify,
+    replace => $managed_replace,
   }
 
 }
